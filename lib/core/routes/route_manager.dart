7a47c67fab8687df0120afa376b327f72743a874
Rishan <shrestha.rishan99@gmail.com>
2025-05-18 08:48:33 +0545

    added missing routes

--- lib/core/routes/route_manager.dart
+++ lib/core/routes/route_manager.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:moviehub/core/routes/routes.dart';
+import 'package:moviehub/feature/auth/view/login_view.dart';
+import 'package:moviehub/feature/home/view/dashboad_view.dart';
 import 'package:moviehub/feature/splash/view/splash_view.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
+    case Routes.loginPage:
+      return buildRoute(LoginView());
+    case Routes.homePage:
+      return buildRoute(DashboadView());
     default:
       return buildRoute(SplashView());
   }
