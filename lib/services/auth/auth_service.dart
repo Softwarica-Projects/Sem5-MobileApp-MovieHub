1b4c1b295f82866c61da55c84f28056d683e2d35
Rishan <shrestha.rishan99@gmail.com>
2025-05-18 08:07:52 +0545

    created a service for register

---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,14 @@
+import 'package:moviehub/demodata.dart';
+import 'package:moviehub/feature/auth/model/login_model.dart';
+import 'package:moviehub/services/core/http_service.dart';
+
+class AuthService {
+  final HttpService _httpService;
+
+  AuthService(this._httpService);
+
+  Future<dynamic> login(LoginModel model) async {
+    await demoDelay();
+    return {"access_token": "dsd"};
+  }
+}
