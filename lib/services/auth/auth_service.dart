122ceb537ae39d26a4e8e286e9653cff59b80773
Rishan <shrestha.rishan99@gmail.com>
2025-07-02 19:02:35 +0545

    cleanup

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,6 +1,6 @@
 import 'package:moviehub/demodata.dart';
-import 'package:moviehub/feature/auth/domain/model/login_model.dart';
-import 'package:moviehub/feature/auth/domain/model/signup_model.dart';
+import 'package:moviehub/feature/auth/domain/entity/login_entity.dart';
+import 'package:moviehub/feature/auth/domain/entity/signup_entity.dart';
 import 'package:moviehub/services/core/http_service.dart';
 
 class AuthService {
@@ -8,12 +8,12 @@ class AuthService {
 
   AuthService(this._httpService);
 
-  Future<dynamic> login(LoginModel model) async {
+  Future<dynamic> login(LoginEntity model) async {
     await demoDelay();
     return {"access_token": "dsd"};
   }
 
-  Future<dynamic> register(SignupModel model) async {
+  Future<dynamic> register(SignupEntity model) async {
     await demoDelay();
     return "done";
   }
